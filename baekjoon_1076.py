37fc43aaee1bf29f496257afdeb53cc4a6efb974
younkyounghwan <youn7117@naver.com>
2018-11-13 20:36:37 +0900

    -18 11 13-

---
+++ baekjoon_1076.py
@@ -0,0 +1,51 @@
+l=[]
+sum = ""
+for i in range(0,3):
+    x = input()
+    l.append(x)
+for i in range(0,2):
+
+    if l[i] == "brown":
+        sum = sum + "1"
+    elif l[i] == "red":
+        sum = sum + "2"
+    elif l[i] == "orange":
+        sum = sum + "3"
+    elif l[i] == "yellow":
+        sum = sum + "4"
+    elif l[i] == "green":
+        sum = sum + "5"
+    elif l[i] == "blue":
+        sum = sum + "6"
+    elif l[i] == "violet":
+        sum = sum + "7"
+    elif l[i] == "grey":
+        sum = sum + "8"
+    elif l[i] == "white":
+        sum = sum + "9"
+
+if l[0] == "black" and l[1] == "black":
+    sum = 0
+
+sum = int(sum)
+if l[2] == "black":
+    sum *= 1
+elif l[2] == "brown":
+    sum *= 10
+elif l[2] == "red":
+    sum *= 100
+elif l[2] == "orange":
+    sum *= 1000
+elif l[2] == "yellow":
+    sum *= 10000
+elif l[2] == "green":
+    sum *= 100000
+elif l[2] == "blue":
+    sum *= 1000000
+elif l[2] == "violet":
+    sum *= 10000000
+elif l[2] == "grey":
+    sum *= 100000000
+elif l[2] == "white":
+    sum *= 1000000000
+print(sum)
---
+++ baekjoon_10869.py
@@ -0,0 +1,8 @@
+x,y=input().split()
+x = int(x)
+y = int(y)
+print(x+y)
+print(x-y)
+print(x*y)
+print(x//y)
+print(x%y)
