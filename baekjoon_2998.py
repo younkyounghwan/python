9dbbcb4660216107a7157940a9e8b1d9bc7fabc8
younkyounghwan <youn7117@naver.com>
2019-03-26 22:32:20 +0900

    -19 03 26-

--- baekjoon_2998.py
+++ baekjoon_2998.py
@@ -1,8 +1,12 @@
-x = input()
-y = list(x[::-1])
+x = list(input())
+y = x[::-1]
+if len(y)%3 == 1:
+    y.append(0)
+    y.append(0)
+if len(y)%3 == 2:
+    y.append(0)
 l = []
 for i in range(0,len(y)//3):
-    z = (x[3*i]*2 +x[3*i+1]*2 + x[3*i+2]*2)
-    l.append(z)
-l = l[::-1]
-print(str(l))
+    z = (int(y[3*i]) +int(y[3*i+1])*2 + int(y[3*i+2])*4)
+    l.append(str(z))
+print("".join(l[::-1]))
