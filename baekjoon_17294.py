1c49be67891ad4c716e8767a06a2cd9db77e382d
younkyounghwan <youn7117@naver.com>
2019-07-31 14:36:37 +0900

    -190731-

--- baekjoon_17294.py
+++ baekjoon_17294.py
@@ -1,1 +1,16 @@
-x = int(input())
+x = list(input())
+for i in range(0,len(x)):
+    x[i] = int(x[i])
+
+diff = 0
+a = 0
+for i in range(0,len(x)-1):
+    if i == 0:
+        diff = x[i] - x[i+1]
+    else:
+        if diff != x[i] - x[i+1]:
+            print("흥칫뿡!! <(￣ ﹌ ￣)>")
+            a += 1
+            break
+if a == 0:
+    print("◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!")
