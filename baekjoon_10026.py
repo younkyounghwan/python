1af892631d02b1bca32d709fae1d6c35dae6d2ff
younkyounghwan <youn7117@naver.com>
2019-08-04 19:11:29 +0900

    -190804-

--- baekjoon_10026.py
+++ baekjoon_10026.py
@@ -1,5 +1,5 @@
 x = int(input())
-l=[]
+l=[[]]*5
 for i in range(0,x):
     y = input()
     l.append([])
---
+++ baekjoon_10026_3.py
@@ -0,0 +1,15 @@
+a = 1
+b = 1.5
+s = "23"
+l = []
+l.append(a)
+l.append(b)
+print(l)
+sum = 0
+for i in range(0,10):
+    sum += i
+print(sum)
+p = [[]]*5
+print(p)
+p[0].append(1)
+print(p)
