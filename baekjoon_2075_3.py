bb4070f3fbb1f2a35ad479f2555d5b9ce6845050
younkyounghwan <youn7117@naver.com>
2019-08-15 19:18:38 +0900

    -190815-

--- baekjoon_2075_3.py
+++ baekjoon_2075_3.py
@@ -1,2 +1,31 @@
-l = list(map(int,input().split()))
-print(l[l.index(min(l))])
+import math
+n = int(input())
+answer = []
+for i in range(math.ceil(n/2)):
+    l = list(map(int,input().split()))
+    if i == 0:
+        pass
+    else:
+        if l.index(max(l)) != l_index:
+            del answer[0]
+    l_index = l.index(max(l))
+    answer.append(max(l))
+for  i in range(n-math.ceil(n/2)):
+    while 1:
+        if len(answer) > n:
+            answer.remove(min(answer))
+        else:
+            break
+
+    l = list(map(int, input().split()))
+
+    while 1:
+        if min(answer) < max(l):
+            answer.append(max(l))
+            l[l.index(max(l))] = 0
+        else:
+            break
+
+for i in range(n-1):
+    answer.remove(max(answer))
+print(max(answer))
---
+++ baekjoon_2075_best.py
@@ -0,0 +1,7 @@
+n=int(input())
+a=[]
+for _ in[0]*n:
+    a+=map(int,input().split());
+    a.sort();
+    a[:-n]=() # 유효값 제시
+print(a[0])
